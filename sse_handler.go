df24189846c5ed2a7965ade7f81bf49056234d46
Toan Nguyen <blackntt@gmail.com>
2019-10-17 22:42:16 +0700

    fix CORS

--- sse_handler.go
+++ sse_handler.go
@@ -101,7 +101,7 @@ func (b *SSEHandler) Subscribe(c *gin.Context) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
-
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	for {
 		msg, open := <-messageChan
 		if !open {
